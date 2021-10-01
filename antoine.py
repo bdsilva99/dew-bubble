417b128f444c0eab771770528219174394140bbb
katmoormann99 <91487157+katmoormann99@users.noreply.github.com>
2021-10-01 17:37:35 -0400

    Update antoine.py

--- antoine.py
+++ antoine.py
@@ -20,6 +20,8 @@ def antoine( a, T ):
     #
     # Code originally by: James C. Sutherland
     # Modified by: Tyler R. Josephson
+    
+    ## what the f is going on 
 
     Ps = 10.0**( a[:,0] - a[:,1] / ( a[:,2] + T ) )
     return Ps
